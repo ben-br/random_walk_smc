fb332b13770713b344f432dff1f40152fe36dc6d
Benjamin Bloem-Reddy <ben-br@github.com>
2018-05-31 18:56:35 +0100

    ne_50 revised

--- prior_test_plots.jl
+++ prior_test_plots.jl
@@ -4,19 +4,20 @@ import Iterators
 using Plots
 gr()
 
-const n_mcmc_iter = 1500 # total number of iterations; includes burn-in
+const n_mcmc_iter = 3000 # total number of iterations; includes burn-in
 const n_burn = 500 # burn-in
 const n_collect = 1 # collect a sample every n_collect iterations
 const n_print = 10 # print progress updates every n_print iterations
 const n_particles = 100
 
-const n_edges_data = 100
+const n_edges_data = 50
 const α = 0.25
 const λ = 4.0
 
-job_id = "2190538"
+job_id = "2190469"
 
 n_samples = convert(Int64,floor((n_mcmc_iter - n_burn)/n_collect))
+n_samples_plot = 1500
 
 # hyperparameter settings
 alpha_hp = [0.5 0.5;
@@ -36,8 +37,8 @@ alpha_s = zeros(Float64,n_samples,n_hp)
 lambda_s = zeros(Float64,n_samples,n_hp)
 tmp = zeros(Float64,n_samples)
 
-dirname1 = "/data/ziz/not-backed-up/bloemred/random_walk_smc/results/prior_sensitivity_100_" * job_id * "/"
-dirname2 = "/data/ziz/bloemred/random_walk_smc/plots/prior/ne_100/"
+dirname1 = "/data/ziz/not-backed-up/bloemred/random_walk_smc/results/prior_sensitivity_" * job_id * "/"
+dirname2 = "/data/ziz/bloemred/random_walk_smc/plots/prior/ne_50/"
 
 for n = 1:n_hp
   fname = "prior_" * job_id * "_" * string(n) * ".jld"
@@ -46,7 +47,7 @@ for n = 1:n_hp
   tmp[:] = load(dirname1 * fname, "lambda_samples")
   lambda_s[:,n] = tmp
   plotname = dirname2 * "prior_" * string(n) * ".pdf"
-  scatter(lambda_s[:,n],alpha_s[:,n],legend=false);
+  scatter(lambda_s[1:n_samples_plot,n],alpha_s[1:n_samples_plot,n],legend=false);
   scatter!([λ],[α],legend=false,c=:black,marker=:star4,ms=10);
   savefig(plotname)
 end
